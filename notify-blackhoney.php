be50dbd7af1ff6989f795cacefe4e35b9f48264a
srsuper <aot5310@gmail.com>
2020-04-24 23:31:20 +0700

    Update notify-blackhoney.php

--- notify-blackhoney.php
+++ notify-blackhoney.php
@@ -23,6 +23,4 @@ function notify_message($message,$token){
  $res = json_decode($result);
  return $res;
 }
-//https://havespirit.blogspot.com/2017/02/line-notify-php.html
-//https://medium.com/@nattaponsirikamonnet/%E0%B8%A1%E0%B8%B2%E0%B8%A5%E0%B8%AD%E0%B8%87-line-notify-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-%E0%B8%9E%E0%B8%B7%E0%B9%89%E0%B8%99%E0%B8%90%E0%B8%B2%E0%B8%99-65a7fc83d97f
 ?>
